8b9d34b4e5d2a14f202b672fd3ab8988edfd7648
LossFunctions <umihuss@gmail.com>
2025-09-18 19:06:40 -0400

    Initial Commit

---
+++ HalalFoodTests/HalalFoodTests.swift
@@ -0,0 +1,17 @@
+//
+//  HalalFoodTests.swift
+//  HalalFoodTests
+//
+//  Created by Umi Hussaini on 9/18/25.
+//
+
+import Testing
+@testable import HalalFood
+
+struct HalalFoodTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
